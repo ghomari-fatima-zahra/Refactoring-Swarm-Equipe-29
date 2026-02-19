168c60448d1294eba784c271e10e7ec8c49e62b1
Maroua925 <nm_ouchefoune@esi.dz>
2026-02-19 19:32:50 +0100

    Fix syntax errors in check_grade function

--- sandbox/messy_code.py
+++ sandbox/messy_code.py
@@ -1,9 +1,9 @@
 def check_grade(score):
-    if score > 0 and score < 50:
+    if score > 0 and score < 50
         return "Fail"
     elif score >= 50 and score < 75:
         return "Pass"
-    elif score >= 75:
+    elif score > 75
         return "Excellent"
     else:
-        return "Invalid"
+        return "Invalid
